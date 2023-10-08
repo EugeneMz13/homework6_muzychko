7e29fa921d7de69b4a92d439414010a6582bd461
Eugene <eugenemz0908@gmail.com>
2023-10-08 15:20:37 +0300

    hometask_6_abstract

---
+++ ClassLibrary_6/Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary_6
+{
+    public abstract class Car
+    {
+        public int Year { get; set; } = 2020;
+        public int NumberOfSeats { get; set; }
+
+        public virtual int GetNumberOfSeats()
+        {
+            return NumberOfSeats;
+        }
+
+        public int GetYear()
+        {
+            return Year;
+        }
+
+    }
+}
---
+++ ClassLibrary_6/Classes/Minivan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_6
+{
+    public class Minivan: Car
+    {
+        public override int GetNumberOfSeats()
+        {
+            return 10;
+        }
+
+        public new int GetYear()
+        {
+            return base.GetYear()+1;
+        }
+
+
+    }
+}
